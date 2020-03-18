6f80ffdb35dfd89a047375ca96f18f526d9e740d
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-03-18 12:07:56 +0300

    add: DepartmentHead

--- Hospital/src/main/java/stuff/Doctor.java
+++ Hospital/src/main/java/stuff/Doctor.java
@@ -5,7 +5,7 @@ import main.java.User;
 public class Doctor extends User {
     String department;
     boolean isHeadOfDepartment;
-    Doctor(String name, String surname, int age, String login, String password, String department, boolean isHeadOfDepartment){
+    public Doctor(String name, String surname, int age, String login, String password, String department, boolean isHeadOfDepartment){
         super(name, surname, age, login, password);
         this.department = department;
         this.isHeadOfDepartment = isHeadOfDepartment;
