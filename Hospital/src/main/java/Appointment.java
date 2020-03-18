afbc0415f92aaf56decba3576bda361d3efbccb4
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-03-18 15:07:49 +0300

    Create Appointment.java

---
+++ Hospital/src/main/java/Appointment.java
@@ -0,0 +1,33 @@
+package main.java;
+
+import main.java.stuff.Doctor;
+
+import java.util.Date;
+
+public class Appointment {
+    private Doctor doctor;
+    private Department department;
+    private Date appDate;
+    Appointment(Doctor doctor, Department department, Date appDate){
+        this.appDate = appDate;
+        this.department = department;
+        this.doctor = doctor;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public Date getAppDate() {
+        return appDate;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setAppDate(Date appDate) {
+        this.appDate = appDate;
+    }
+    
+}
