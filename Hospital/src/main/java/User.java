8d97deb2467db988ac82fe44bd5eb5af14514b3c
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-03-18 10:44:05 +0300

    Create User.java

---
+++ Hospital/src/main/java/User.java
@@ -0,0 +1,48 @@
+package main.java;
+
+public class User {
+    private String name;
+    private String surname;
+    private int age;
+    private String login;
+    private String password;
+
+    User(String name, String surname, int age, String login, String password){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void logOut(){
+        System.out.println("Пользователь вышел из приложения");
+    }
+}
