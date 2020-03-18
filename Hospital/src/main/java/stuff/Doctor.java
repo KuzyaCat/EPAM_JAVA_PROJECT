e888eb47c5183f1266d54ae44d33eb1220ba3019
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-03-18 11:55:53 +0300

    Update Doctor.java

--- Hospital/src/main/java/stuff/Doctor.java
+++ Hospital/src/main/java/stuff/Doctor.java
@@ -1,4 +1,17 @@
 package main.java.stuff;
 
-public class Doctor {
+import main.java.User;
+
+public class Doctor extends User {
+    String department;
+    boolean isHeadOfDepartment;
+    Doctor(String name, String surname, int age, String login, String password, String department, boolean isHeadOfDepartment){
+        super(name, surname, age, login, password);
+        this.department = department;
+        this.isHeadOfDepartment = isHeadOfDepartment;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }
