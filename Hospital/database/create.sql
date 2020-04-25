0acd4ed7d1fb70660d3f9dbf10ed8e53ba190676
unknown <vlad.raba@mail.ru>
2020-04-25 17:15:29 +0300

    update: sql scripts

--- Hospital/database/create.sql
+++ Hospital/database/create.sql
@@ -6,7 +6,6 @@ CREATE TABLE PATIENT
 , AGE INT NOT NULL 
 , LOGIN VARCHAR(20) NOT NULL 
 , PASSWORD VARCHAR(20) NOT NULL 
-, DOCTOR_ID INT NOT NULL 
 , RECOVERED BOOLEAN NOT NULL 
 , CONSTRAINT PATIENT_PK PRIMARY KEY 
   (
@@ -15,10 +14,10 @@ CREATE TABLE PATIENT
 );
 
 INSERT INTO PATIENT
-VALUES (1, 'Anatoliy', 'Tsoy', 55,'tsoyzhiv','kukushka', 1,true);
+VALUES (1, 'Anatoliy', 'Tsoy', 55,'tsoyzhiv','kukushka', true);
 
 INSERT INTO PATIENT
-VALUES (2, 'Sergey', 'Trofimov', 44,'konyachok','shashlichok', 2, false);
+VALUES (2, 'Sergey', 'Trofimov', 44,'konyachok','shashlichok', false);
 
 
 CREATE TABLE NURSE 
@@ -29,7 +28,6 @@ CREATE TABLE NURSE
 , AGE INT NOT NULL
 , LOGIN VARCHAR(20) NOT NULL 
 , PASSWORD VARCHAR(20) NOT NULL 
-, PATIENT_ID INT 
 , CONSTRAINT NURSE_PK PRIMARY KEY 
   (
     ID_NURSE 
@@ -37,10 +35,10 @@ CREATE TABLE NURSE
 );
 
 INSERT into Nurse
-values(1,'Mariya', 'Sharapova', 21, 'sugarpova','byumysweets', 1);
+values(1,'Mariya', 'Sharapova', 21, 'sugarpova','byumysweets');
 
 INSERT into Nurse
-values(2,'Lyubov', 'Sharipova', 40, 'lushar','qqqq', 2);
+values(2,'Lyubov', 'Sharipova', 40, 'lushar','qqqq');
 
 CREATE TABLE DOCTOR 
 (
@@ -50,7 +48,6 @@ CREATE TABLE DOCTOR
 , AGE INT NOT NULL
 , LOGIN VARCHAR(20) NOT NULL 
 , PASSWORD VARCHAR(20) NOT NULL 
-, PATIENT_ID INT NOT NULL 
 , DEPARTMENT VARCHAR(20) NOT NULL
 , IS_HEAD_OF_DEPARTMENT BOOLEAN NOT NULL
 , CONSTRAINT DOCTOR_PK PRIMARY KEY 
@@ -60,9 +57,9 @@ CREATE TABLE DOCTOR
 );
 
 Insert into DOCTOR
-values(1,'Boris', 'Levin', 22, 'borlev','12345', 1, 'neurology', false);
+values(1,'Boris', 'Levin', 22, 'borlev','12345', 'neurology', false);
 Insert into DOCTOR
-values(2,'Semen', 'Lobanov', 31, 'semlob', '54321', 2, 'traumatology', false);
+values(2,'Semen', 'Lobanov', 31, 'semlob', '54321', 'traumatology', false);
 
 
 CREATE TABLE TREATMENT (
@@ -80,15 +77,11 @@ CREATE TABLE TREATMENT (
 insert into treatment
 values(1, 'Computer Games', 'DOTA', 'Destroy opponents', 'Ospa', 1);
 
-insert into treatment
-values(2, 'Computer Games', 'CS', 'Go na midok', 'Orvi', 2);
-
 CREATE TABLE APPOINTMENT 
 (
   ID_APPOINTMENT INT NOT NULL 
 , ID_DOCTOR INT NOT NULL 
 , ID_PATIENT INT NOT NULL 
-, ID_DIAGNOSE INT NOT NULL 
 , APPOINTMENT_DATE DATE NOT NULL 
 , CONSTRAINT APPOINTMENT_PK PRIMARY KEY 
   (
@@ -97,18 +90,12 @@ CREATE TABLE APPOINTMENT
 );
 
 insert into appointment
-values(1,1,1,1,'2019-12-11');
+values(1, 1, 1, '2019-12-11');
 insert into appointment
-values(2,2,2,2,'2020-01-13');
+values(2, 2, 2, '2020-01-13');
 
-ALTER TABLE NURSE ADD CONSTRAINT fk_NURSE_PATIENT_ID FOREIGN KEY(PATIENT_ID)
+ALTER TABLE APPOINTMENT ADD CONSTRAINT fk_APPOINTMENT_ID_PATIENT FOREIGN KEY(ID_PATIENT)
 REFERENCES PATIENT (ID_PATIENT);
-
-ALTER TABLE DOCTOR ADD CONSTRAINT fk_DOCTOR_PATIENT_ID FOREIGN KEY(PATIENT_ID)
-REFERENCES PATIENT (ID_PATIENT);
-
-ALTER TABLE Appointment ADD CONSTRAINT fk_Appointment_ID_PATIENT FOREIGN KEY(ID_PATIENT)
-REFERENCES Patient (ID_Patient);
 
 ALTER TABLE TREATMENT ADD CONSTRAINT fk_TREATMENT_ID_APPOINTMENT FOREIGN KEY(ID_APPOINTMENT)
 REFERENCES APPOINTMENT (ID_APPOINTMENT);
