3e90104f2aaf4e98363633d702628dcabac55f14
Alexander <lizerd34@mail.ru>
2020-04-26 20:12:14 +0300

    feat: add AppointmentSearcher class

--- Hospital/src/main/java/Main.java
+++ Hospital/src/main/java/Main.java
@@ -1,13 +1,6 @@
 package main.java;
 
-import main.java.components.Appointment;
 import main.java.controllers.menus.MainMenu;
-import main.java.users.Patient;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
