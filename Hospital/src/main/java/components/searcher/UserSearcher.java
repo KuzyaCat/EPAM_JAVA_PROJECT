0d3eb4ed89aa44989a72c2f286deb873b89fc9c2
Alexander <lizerd34@mail.ru>
2020-04-26 16:29:59 +0300

    feat: add searching users by first name

--- Hospital/src/main/java/components/searcher/UserSearcher.java
+++ Hospital/src/main/java/components/searcher/UserSearcher.java
@@ -3,11 +3,21 @@ package main.java.components.searcher;
 import main.java.users.User;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UserSearcher {
     protected List<User> userList;
 
     public UserSearcher(List<User> userList) {
         this.userList = userList;
+    }
+
+    public List<User> findUsersByFirstName(String firstName) {
+        Stream<User> userStream = this.userList.stream();
+
+        return userStream
+                .filter(u -> u.getName().equals(firstName))
+                .collect(Collectors.toList());
     }
 }
